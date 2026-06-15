b187398827f1ce4c1a36b8bdc9e5c6492d4d19dd
sachiolucky0809-ux <sachio.lucky.0809@gmail.com>
2026-06-15 12:23:41 +0900

    Add connection memo tool as installable PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'renraku-memo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './vendor/html2pdf.bundle.min.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (event.request.method === 'GET' && response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
